f66439e7d85d67f873b7024be798e71d6e1bea03
Solquarters <rom.kabucov@gmail.com>
2024-10-04 16:31:00 +0200

    added Formmodels and date transform

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from '../../models/user.class';
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class DialogAddUserComponent {
 
+  user: User = new User();
+
+  birthDate: Date | undefined;
+
+  saveUser(){
+    if(this.birthDate){
+      this.user.birthDate = this.birthDate.getTime();
+    }
+    console.log(this.user);
+
+  }
 }
