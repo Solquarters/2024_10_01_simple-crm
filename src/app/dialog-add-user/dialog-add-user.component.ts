be4541cfece643142074d0d79271e32fc6f2f9b1
Solquarters <rom.kabucov@gmail.com>
2024-10-22 11:47:37 +0200

    outsourced safeUser function from single component to firestore service to enable simple reuse in other components

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,39 +1,93 @@
+// import { Component } from '@angular/core';
+// import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+// import { User } from '../../models/user.class';
+// import {MatDialogRef} from '@angular/material/dialog';
+
+// @Component({
+//   selector: 'app-dialog-add-user',
+//   templateUrl: './dialog-add-user.component.html',
+//   styleUrls: ['./dialog-add-user.component.scss']
+// })
+
+// export class DialogAddUserComponent {
+//   firestore: Firestore;
+
+//   loading = false;
+
+//   user: User = new User();
+//   birthDate: Date | undefined;
+
+//   constructor(firestore: Firestore, public dialogRef: MatDialogRef<DialogAddUserComponent>) {
+//     this.firestore = firestore;
+//   }
+
+//   saveUser() {
+//     if (this.birthDate) {
+//       this.user.birthDate = this.birthDate.getTime();
+//     }
+//     this.loading = true;
+//     const userRef = collection(this.firestore, 'users');
+//     addDoc(userRef, this.user.toJSON())
+//       .then((result: any) => {
+//         console.log('Added user to Firestore', result);
+//         this.loading = false;
+//         this.dialogRef.close();
+//       })
+//       .catch((error) => {
+//         console.error('Error adding user to Firestore', error);
+//       });
+//   }
+
+//   cancelDialog(){
+//     this.dialogRef.close();
+//   }
+// }
+
+
 import { Component } from '@angular/core';
 import { Firestore, collection, addDoc } from '@angular/fire/firestore';
 import { User } from '../../models/user.class';
 import {MatDialogRef} from '@angular/material/dialog';
+import { FirestoreService } from '../firestore.service';
 
 @Component({
   selector: 'app-dialog-add-user',
   templateUrl: './dialog-add-user.component.html',
   styleUrls: ['./dialog-add-user.component.scss']
 })
+
 export class DialogAddUserComponent {
-  firestore: Firestore;
+  // firestore: Firestore;
 
-  loading = false;
+  // loading = false;
 
-  user: User = new User();
-  birthDate: Date | undefined;
+  // user: User = new User();
+  // birthDate: Date | undefined;
 
-  constructor(firestore: Firestore, public dialogRef: MatDialogRef<DialogAddUserComponent>) {
-    this.firestore = firestore;
+  //cinstructo input firestore: Firestore,
+  constructor(public dialogRef: MatDialogRef<DialogAddUserComponent>, public firestoreService: FirestoreService) {
+    // this.firestore = firestore;
   }
-  saveUser() {
-    if (this.birthDate) {
-      this.user.birthDate = this.birthDate.getTime();
-    }
-    this.loading = true;
-    const userRef = collection(this.firestore, 'users');
-    addDoc(userRef, this.user.toJSON())
-      .then((result: any) => {
-        console.log('Added user to Firestore', result);
-        this.loading = false;
-        this.dialogRef.close();
-      })
-      .catch((error) => {
-        console.error('Error adding user to Firestore', error);
-      });
+  
+  // saveUser() {
+  //   if (this.birthDate) {
+  //     this.user.birthDate = this.birthDate.getTime();
+  //   }
+  //   this.loading = true;
+  //   const userRef = collection(this.firestore, 'users');
+  //   addDoc(userRef, this.user.toJSON())
+  //     .then((result: any) => {
+  //       console.log('Added user to Firestore', result);
+  //       this.loading = false;
+  //       this.dialogRef.close();
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error adding user to Firestore', error);
+  //     });
+  // }
+
+  saveUser(){
+    this.firestoreService.saveUser(this.dialogRef);
   }
 
   cancelDialog(){
