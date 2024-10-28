d3bbdaaab298e04cd5d69552bae19ed5583d75d8
Solquarters <rom.kabucov@gmail.com>
2024-10-28 21:27:18 +0100

    faulty push! add-user-component.ts und html uncomplete - check the comment on top of the ts file

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -4,7 +4,14 @@ import {MatDialogRef} from '@angular/material/dialog';
 import { MatDialog } from '@angular/material/dialog';
 import { FirestoreService } from '../firestore.service';
 import { AddLicenceDialogComponent } from '../add-licence-dialog/add-licence-dialog.component';
-import { FormBuilder, Validators, FormControl } from '@angular/forms';
+import { Validators, FormControl } from '@angular/forms';
+
+
+
+/////BEIM ÖFFNEN VON ADD USER DIALOG NULL INJECTOR ERROR zu NGCONTROL
+///AUSSERDEM HABE ICH BISHER IM HTML NUR FIRSTNAME LASTNAME verlinkt mit formControl="firstNameControl" und hier auch ohne eckige klammern [formControl]="firstNameControl"
+//da ein fehler ausgelöst wurde mit den klammern ... ic hweiß noch nicht wieso
+import { NgControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -17,23 +24,65 @@ export class DialogAddUserComponent {
  
   constructor(public dialogRef: MatDialogRef<DialogAddUserComponent>, 
     public firestoreService: FirestoreService, 
-    public dialog: MatDialog,
-    private formbuilder: FormBuilder
+    public dialog: MatDialog
   ) {
     this.firestoreService.user = new User();
     this.firestoreService.birthDate = new Date('')
   }
   
-  addUserInputValidation = this.formbuilder.group({
-    firstNameTest:['', Validators.required]
-  });
+ ////////////////////////////////////////
 
+ firstNameControl = new FormControl('', [Validators.required]);
+ lastNameControl = new FormControl('', [Validators.required]);
+ emailControl = new FormControl('', [Validators.required, Validators.email]);
+ streetControl = new FormControl('', [Validators.required]);
+ zipCodeControl = new FormControl( 0 , [Validators.required]);
+ cityControl = new FormControl('', [Validators.required]);
+
+
+
+ ////////////////////////////////////////
   // onSubmit(ngForm: NgForm){
 
   // }
 
-  saveUser(){
-    this.firestoreService.saveUser(this.dialogRef);
+  // saveUser(){
+  //   this.firestoreService.saveUser(this.dialogRef);
+  // }
+
+  saveUser() {
+    if(this.firstNameControl.value && 
+      this.lastNameControl.value &&
+      this.emailControl.value &&
+      this.streetControl.value &&
+      this.zipCodeControl.value &&
+      this.cityControl.value
+    
+    ){
+      if (
+        this.firstNameControl.valid &&
+        this.lastNameControl.valid &&
+        this.emailControl.valid &&
+        this.streetControl.valid &&
+        this.zipCodeControl.valid &&
+        this.cityControl.valid
+      ) {
+        // Update the user object with FormControl values
+        this.firestoreService.user.firstName = this.firstNameControl.value;
+        this.firestoreService.user.lastName = this.lastNameControl.value;
+        this.firestoreService.user.email = this.emailControl.value;
+        this.firestoreService.user.street = this.streetControl.value;
+        this.firestoreService.user.zipCode = this.zipCodeControl.value;
+        this.firestoreService.user.city = this.cityControl.value;
+  
+        // Call the saveUser function in the FirestoreService
+        this.firestoreService.saveUser(this.dialogRef);
+      } else {
+        // Handle invalid form (e.g., show an error message)
+        console.error('Form is invalid');
+      }
+    }
+    
   }
 
   cancelDialog(){
