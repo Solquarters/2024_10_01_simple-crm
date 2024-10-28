b7452c1f1b133260a550fcb6c6f8427ea0da20f8
Solquarters <rom.kabucov@gmail.com>
2024-10-28 22:25:43 +0100

    form validation works as a group validation 2

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -9,12 +9,6 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 
 
-
-/////BEIM ÖFFNEN VON ADD USER DIALOG NULL INJECTOR ERROR zu NGCONTROL
-///AUSSERDEM HABE ICH BISHER IM HTML NUR FIRSTNAME LASTNAME verlinkt mit formControl="firstNameControl" und hier auch ohne eckige klammern [formControl]="firstNameControl"
-//da ein fehler ausgelöst wurde mit den klammern ... ic hweiß noch nicht wieso
-
-
 @Component({
   selector: 'app-dialog-add-user',
   templateUrl: './dialog-add-user.component.html',
@@ -47,15 +41,7 @@ export class DialogAddUserComponent {
   }
 
 
-  
- ////////////////////////////////////////
 
-//  firstNameControl = new FormControl('', [Validators.required]);
-//  lastNameControl = new FormControl('', [Validators.required]);
-//  emailControl = new FormControl('', [Validators.required, Validators.email]);
-//  streetControl = new FormControl('', [Validators.required]);
-//  zipCodeControl = new FormControl( 0 , [Validators.required]);
-//  cityControl = new FormControl('', [Validators.required]);
 get firstNameControl(): FormControl {
   return this.userForm.get('firstNameControl') as FormControl;
 }
@@ -80,12 +66,6 @@ get cityControl(): FormControl {
   return this.userForm.get('cityControl') as FormControl;
 }
 
-
- ////////////////////////////////////////
-  // onSubmit(ngForm: NgForm){
-
-  // }
-
   // saveUser(){
   //   this.firestoreService.saveUser(this.dialogRef);
   // }
@@ -108,44 +88,7 @@ get cityControl(): FormControl {
       console.error('Form is invalid');
     }
   }
-  // saveUser() {
-  //   if(this.firstNameControl.value && 
-  //     this.lastNameControl.value &&
-  //     this.emailControl.value &&
-  //     this.streetControl.value &&
-  //     this.zipCodeControl.value &&
-  //     this.cityControl.value
-    
-  //   ){
-  //     if (
-  //       this.firstNameControl.valid &&
-  //       this.lastNameControl.valid &&
-  //       this.emailControl.valid &&
-  //       this.streetControl.valid &&
-  //       this.zipCodeControl.valid &&
-  //       this.cityControl.valid
-  //     ) {
-  //       // Update the user object with FormControl values
-  //       this.firestoreService.user.firstName = this.firstNameControl.value;
-  //       this.firestoreService.user.lastName = this.lastNameControl.value;
-  //       this.firestoreService.user.email = this.emailControl.value;
-  //       this.firestoreService.user.street = this.streetControl.value;
-  //       this.firestoreService.user.zipCode = this.zipCodeControl.value;
-  //       this.firestoreService.user.city = this.cityControl.value;
-  
-  //       // Call the saveUser function in the FirestoreService
-  //       this.firestoreService.saveUser(this.dialogRef);
-  //     } else {
-  //       // Handle invalid form (e.g., show an error message)
-  //       console.error('Form is invalid');
-  //     }
-  //   }
-  //   else{
-      
-  //   }
-    
-  // }
-
+ 
   cancelDialog(){
     this.dialogRef.close();
   }
