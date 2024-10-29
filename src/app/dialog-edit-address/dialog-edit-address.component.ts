a3b3c183726ab560a22bd642a53a03c8930b15bf
Solquarters <rom.kabucov@gmail.com>
2024-10-29 11:42:37 +0100

    form validation complete

--- src/app/dialog-edit-address/dialog-edit-address.component.ts
+++ src/app/dialog-edit-address/dialog-edit-address.component.ts
@@ -1,19 +1,10 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-dialog-edit-address',
-//   templateUrl: './dialog-edit-address.component.html',
-//   styleUrl: './dialog-edit-address.component.scss'
-// })
-// export class DialogEditAddressComponent {
-
-// }
-
 
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { User } from '../../models/user.class';
 import { FirestoreService } from '../firestore.service';
+
+import { FormControl, Validators, FormGroup, FormBuilder  } from '@angular/forms';
 
 @Component({
   selector: 'app-dialog-edit-address',
@@ -24,26 +15,67 @@ export class DialogEditAddressComponent {
 
 user: User = new User();
 // loading = false;
+userForm: FormGroup;
+
 
 constructor(
   public dialogRef: MatDialogRef<DialogEditAddressComponent>,
-  public firestoreService: FirestoreService  
+  public firestoreService: FirestoreService ,
+  private fb: FormBuilder,
 ){
 
+
+  this.userForm = this.fb.group({
+    streetControl: ['', Validators.required],
+    zipCodeControl: ['', Validators.required],
+    cityControl: ['', Validators.required],
+  });
+}
+
+
+get streetControl(): FormControl {
+  return this.userForm.get('streetControl') as FormControl;
+}
+
+get zipCodeControl(): FormControl {
+  return this.userForm.get('zipCodeControl') as FormControl;
+}
+
+get cityControl(): FormControl {
+  return this.userForm.get('cityControl') as FormControl;
 }
 
 cancelDialog(){
   this.dialogRef.close();
 }
 
-async saveUser(){
-  this.firestoreService.user = this.user;
-
-  if(this.user.id){
-    await this.firestoreService.updateUser(this.user.id );
+async saveUser() {
+  if (this.userForm.valid) {
+    // Update the user object with FormControl values
+    const formValues = this.userForm.value;
+    this.firestoreService.user.street = formValues.streetControl;
+    this.firestoreService.user.zipCode = formValues.zipCodeControl;
+    this.firestoreService.user.city = formValues.cityControl;
+    if(this.user.id){
+      await this.firestoreService.updateUser(this.user.id);
+    }
+    this.dialogRef.close();
+  } else {
+    // Mark all controls as touched to trigger validation messages
+    this.userForm.markAllAsTouched();
+    console.error('Form is invalid');
   }
-
-  this.dialogRef.close();
 }
+
+
+
+// async saveUser(){
+//   this.firestoreService.user = this.user;
+
+//   if(this.user.id){
+//     await this.firestoreService.updateUser(this.user.id );
+//   }
+//   this.dialogRef.close();
+// }
 
 }
