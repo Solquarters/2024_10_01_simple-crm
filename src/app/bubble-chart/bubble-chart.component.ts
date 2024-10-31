e47e705d692da9d8037d8520e0338924211e4d5a
Solquarters <rom.kabucov@gmail.com>
2024-10-31 19:32:50 +0100

    saving before D3 implementation

---
+++ src/app/bubble-chart/bubble-chart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bubble-chart',
+  templateUrl: './bubble-chart.component.html',
+  styleUrl: './bubble-chart.component.scss'
+})
+export class BubbleChartComponent {
+
+}
